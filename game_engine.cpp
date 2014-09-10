f7f9750f3a0bb12ff55a5f9ef6c5bbfbb07cb46f
Marko Mäkilä <maruchan86@gmail.com>
2014-09-10 11:30:37 +0300

    battle function in main()

--- game_engine.cpp
+++ game_engine.cpp
@@ -7,11 +7,11 @@ int GameEngine::doDamage(int accuracy) {
 
     random_hit = rand()%100;
     if (random_hit <= accuracy) {
-        if (random_hit <= 20) { return 15; }
-        if (random_hit > 20 && random_hit <= 40) { return 20; }
-        if (random_hit > 40 && random_hit <= 60) { return 15; }
-        if (random_hit > 60 && random_hit <= 80) { return 30; }
-        if (random_hit > 80 && random_hit <= 100) { return 45; }
+        if (random_hit <= 20) { return 15; } //leg hit
+        if (random_hit > 20 && random_hit <= 40) { return 0; } //Missed shot
+        if (random_hit > 40 && random_hit <= 60) { return 15; } //Arm hit
+        if (random_hit > 60 && random_hit <= 95) { return 30; }  //Torso hit
+        if (random_hit > 95 && random_hit <= 100) { return 100; } //Headshot
     } else {
         return 0;
     }
--- main.cpp
+++ main.cpp
@@ -3,33 +3,35 @@
 #include "game_engine.h"
 #include "players.h"
 
-int main(void) { 
+int battle()
+{
 	srand(time(NULL)); // initializing the random seed
-
 	GameEngine engine;
 	Players p;
 
 	p.setHealth(100);
-
-	while (p.getHealth(0) >= 1 or p.getHealth(1) >= 1)
+	int damage;
+	while (p.getHealth(0) >= 1 || p.getHealth(1) >= 1)
 	{
 		int rTurn = rand()%2;
 		if (rTurn == 0) // 0 = player, 1 = cpu
 		{
-			if (p.getHealth(0) >= 1) {
-				p.reduceHealth(engine.doDamage(80),1);
-				//std::cout << "Player health: "<< p.getHealth(0) << std::endl; 
+			if (p.getHealth(0) > 1) {
+				damage = engine.doDamage(100);
+				p.reduceHealth(damage,1);
+				std::cout << "Player did damage: "<< damage << std::endl; 
 			} else { break; }
 		}
 		else
 		{
-			if (p.getHealth(1) >= 1) {
-				p.reduceHealth(engine.doDamage(80),0);
-				//std::cout << "CPU health: "<< p.getHealth(1) << std::endl;
+			if (p.getHealth(1) > 1) {
+				damage = engine.doDamage(100);
+				p.reduceHealth(damage,0);
+				std::cout << "CPU did damage: "<< damage << std::endl;
 			} else { break; }
 		}
 	}
-
+	std::cout << "=============================" << std::endl;
 	if (p.getHealth(0) > p.getHealth(1))
 	{
 		std::cout << "PLAYER WINS" << std::endl;
@@ -48,6 +50,13 @@ int main(void) {
 		std::cout << "Player health: " << p.getHealth(0) << std::endl;
 		std::cout << "CPU health: " << p.getHealth(1) << std::endl;
 	}
-	return(0); 
+}
 
+int main(void) { 
+	battle();
+	char goOn;
+	std::cout << "continue? (y/n)" << std::endl;
+	std::cin >> goOn;
+	if (goOn == 'y') { battle(); }
+	else { return(0); }
 }
